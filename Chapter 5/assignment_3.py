31789af890b60c32019ecc2247326131c4dea22e
Sabih <sabihhasan12@hotmail.co.uk>
2018-07-07 09:28:57 +0100

    sabih

--- Chapter 5/assignment_3.py
+++ Chapter 5/assignment_3.py
@@ -1,41 +1,44 @@
 import sys
+import os
 # from assignment_3 import ConfigDict
 
 class ConfigDict(dict):
-	"""docstring for ConfigDict"""
+	
 	def __init__(self, filepath):
-		config = open(filepath)
 
-		config_dict = {}
-		for line in config:
-			key_value = line.split('=')
-			key = key_value[0]
-			value = key_value[1]
-			config_dict[key] = value.strip()
-
-		print(config_dict)
+		self._filepath = filepath
+		if os.path.exists(self._filepath):
+			with open(self._filepath) as f:
+				for line in f:
+					line = line.rstrip()
+					key, value = line.split('=', 1)
+					dict.__setitem__(self, key, value)
 
 
 	def __setitem__(self, key, val):
+
 		dict.__setitem__(self, key, val)
-		
+		with open(self._filepath, 'w') as fh:
+			for key, value in self.items():
+				fh.write('{0}={1}\n'.format(key, value))
+
+
 
 cd = ConfigDict('config.txt')
 
+if len(sys.argv) == 3:
 
-# if len(sys.argv) == 3:
+    key = sys.argv[1]
+    value = sys.argv[2]
+    print('writing data:  {0}, {1}'.format(key, value))
 
-#     key = sys.argv[1]
-#     value = sys.argv[2]
-#     print('writing data:  {0}, {1}'.format(key, value))
+    cd[key] = value
 
-#     cd[key] = value
+else:
 
-# else:
-
-#     print('reading data')
-#     for key in cd.keys():
-#         print('   {0} = {1}'.format(key, cd[key]))
+    print('reading data')
+    for key in cd.keys():
+        print('   {0} = {1}'.format(key, cd[key]))
 
 
 
